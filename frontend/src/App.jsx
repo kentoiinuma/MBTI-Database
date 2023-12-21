f654d7f70686046efadd57e22731fe37b0bbba3a
kentoiinuma <kento31103@icloud.com>
2023-12-21 10:09:26 +0900

    Header_after_login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { ClerkProvider, SignedIn } from '@clerk/clerk-react';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import SignInPage from './components/SignInPage'; // サインインページのコンポーネントをインポート
 
 const clerk_pub_key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function App() {
   return (
     <ClerkProvider publishableKey={clerk_pub_key}>
-      <Router>
-        <div className="flex h-screen">
-          <Sidebar />
-          <div className="flex flex-col flex-1">
-            <Header />
-            <main className="flex-1 overflow-auto">
-              <SignedIn>
-                {/* ログイン済みの場合に表示するコンテンツ */}
-              </SignedIn>
-              <Route path="/signin" component={SignInPage} />
-            </main>
-          </div>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+          <Header />
+          <main className="flex-1 overflow-auto">
+            <SignedIn>
+              {/* ログイン済みの場合に表示するコンテンツ */}
+            </SignedIn>
+            <SignedOut>
+              
+            </SignedOut>
+          </main>
         </div>
-      </Router>
+      </div>
     </ClerkProvider>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,22 @@
-// Header.jsx
 import React from 'react';
-import { useUser, UserButton } from '@clerk/clerk-react';
-import { useNavigate } from "react-router-dom";
+import { useUser, SignInButton, SignOutButton } from '@clerk/clerk-react';
 
 const Header = () => {
   const { isSignedIn, user } = useUser();
-  const navigate = useNavigate();
 
   return (
     <header className="flex justify-between items-center p-4 bg-white text-black border-b border-[#7DB9DE]">
-      {/* isSignedInがtrueの場合にユーザー名とUserButtonを表示 */}
       {isSignedIn ? (
         <>
           <span className="text-xl font-semibold">{user?.firstName}</span>
           <div className="flex items-center">
-            {/* 他のボタンやコンテンツをここに配置 */}
-            <UserButton /> {/* Clerkから提供されるユーザーアイコン */}
+            <SignOutButton>SignOut</SignOutButton>
           </div>
         </>
       ) : (
         <div className="ml-auto">
-          {/* サインインしていないときに表示する要素 */}
           <button className="btn btn-primary mr-4">?</button>
-          {/* サインイン画面を開くボタン */}
-          <button className="btn btn-primary" onClick={() => navigate('/signin')}>⇒</button>
+          <SignInButton>SignIn</SignInButton>
         </div>
       )}
     </header>
@@ -31,3 +24,6 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
