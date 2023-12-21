c18273df8fcd36858d4fbb446653233c2053be17
kentoiinuma <kento31103@icloud.com>
2023-12-21 20:39:45 +0900

    aaa

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#
 ActiveRecord::Schema[7.0].define(version: 2023_12_11_153739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
