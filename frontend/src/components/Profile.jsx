cbcd86549053538babcd314f949577637e9022c8
kentoiinuma <kento31103@icloud.com>
2024-01-05 08:23:54 +0900

    drop-down

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -15,15 +15,12 @@ const Profile = () => {
     }
   }, [user]);
 
-  // 初期状態で 'posts' セクションが選択されているように設定
   const [selectedSection, setSelectedSection] = useState('posts');
 
-  // セクションを選択する関数
   const selectSection = (section) => {
     setSelectedSection(section);
   };
 
-  // 選択されたセクションに基づいてスタイルを適用する関数
   const getSelectedStyle = (section) => {
     if (selectedSection === section) {
       return {
@@ -36,7 +33,6 @@ const Profile = () => {
     return {};
   };
 
-  // 選択されたセクションに基づいてコンテンツを表示する関数
   const renderContent = () => {
     switch (selectedSection) {
       case 'posts':
@@ -52,33 +48,51 @@ const Profile = () => {
 
   return (
     <div className="flex flex-col items-center mt-8">
-      <div className="flex items-center justify-start w-full px-8">
-        <div className="h-24 w-24 bg-gray-200 rounded-full">
-          <img src={user?.profileImageUrl} alt="User avatar" className="h-24 w-24 rounded-full" />
-        </div>
-        <div className="ml-8">
-          <h1 className="text-xl">{user.username}</h1>
-          <div className="text-xl">{mbtiType}</div>
-        </div>
-      </div>
-      <div className="flex justify-between items-center mt-16 w-full">
-        <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('posts')}>
-          <span className="text-xl">ポスト</span>
-          <div style={getSelectedStyle('posts')}></div>
-        </div>
-        <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('comments')}>
-          <span className="text-xl">コメント</span>
-          <div style={getSelectedStyle('comments')}></div>
-        </div>
-        <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('likes')}>
-          <span className="text-xl">いいね</span>
-          <div style={getSelectedStyle('likes')}></div>
-        </div>
-      </div>
-      <hr className="border-t border-[#7DB9DE] w-full" />
-      {renderContent()}
+      {user && (
+        <>
+          <div className="flex items-center justify-between w-full px-8">
+            <div className="h-24 w-24 bg-gray-200 rounded-full">
+              <img src={user?.profileImageUrl} alt="User avatar" className="h-24 w-24 rounded-full" />
+            </div>
+            <div className="ml-8">
+              <h1 className="text-xl">{user.username}</h1>
+              <div className="text-xl">{mbtiType}</div>
+            </div>
+            <div className="ml-auto mb-12 mr-20">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                  </svg>
+                </div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                  <li><a>アイコン/アバター</a></li>
+                  <li><a>MBTIタイプ/診断方法</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center mt-16 w-full">
+            <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('posts')}>
+              <span className="text-xl">ポスト</span>
+              <div style={getSelectedStyle('posts')}></div>
+            </div>
+            <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('comments')}>
+              <span className="text-xl">コメント</span>
+              <div style={getSelectedStyle('comments')}></div>
+            </div>
+            <div className="flex-1 text-center cursor-pointer" onClick={() => selectSection('likes')}>
+              <span className="text-xl">いいね</span>
+              <div style={getSelectedStyle('likes')}></div>
+            </div>
+          </div>
+          <hr className="border-t border-[#7DB9DE] w-full" />
+          {renderContent()}
+        </>
+      )}
     </div>
   );
 };
 
 export default Profile;
+
