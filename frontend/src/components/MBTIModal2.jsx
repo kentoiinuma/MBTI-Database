d036d277d9c7645675ee8f38773765491983ccf7
kentoiinuma <kento31103@icloud.com>
2024-01-05 17:40:07 +0900

    閉じるボタン2

--- frontend/src/components/MBTIModal2.jsx
+++ frontend/src/components/MBTIModal2.jsx
@@ -38,8 +38,8 @@ const MBTIModal = ({ onClose, onUpdate }) => {
       setMethodError(false);
     }
     if (selectedMBTI && diagnosisMethod) {
-      const response = await fetch(`${API_URL}/api/v1/mbti/${user.id}`, { // ユーザーIDをURLに追加
-        method: 'PUT', // POSTからPUTに変更
+      const response = await fetch(`${API_URL}/api/v1/mbti/${user.id}`, {
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json'
         },
@@ -53,88 +53,85 @@ const MBTIModal = ({ onClose, onUpdate }) => {
         // エラーハンドリング
       }
 
-      onUpdate(selectedMBTI); // 親コンポーネントの状態を更新
-      onClose(); // モーダルを閉じます
+      onUpdate(selectedMBTI);
+      onClose();
     } 
   };
 
   return (
     <>
-        <div style={{ textAlign: 'right' }}> {/* Added */}
-        <button className="btn btn-circle btn-outline" style={{ marginRight: '32px' }} onClick={onClose}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-        </div>
-        <div className="fixed inset-0 z-50 flex items-center justify-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}>
+
+      <div className="fixed inset-0 z-50 flex items-center justify-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}>
         <div className="bg-white p-6 rounded-lg shadow-xl max-w-lg mx-auto" style={{ borderColor: '#2EA9DF' }}>
-            {mbtiError && (
+        <div style={{ textAlign: 'right' }}>
+        <button onClick={onClose}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+          {mbtiError && (
             <div role="alert" className="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
-            <span>MBTIタイプを選択してください</span>
+              </svg>
+              <span>MBTIタイプを選択してください</span>
             </div>
-            )}
-            {methodError && (
+          )}
+          {methodError && (
             <div role="alert" className="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-            </svg>
-            <span>タイプ診断の方法を選択してください。</span>
+              </svg>
+              <span>タイプ診断の方法を選択してください。</span>
             </div>
-            )}
-            <form onSubmit={handleSubmit}>
+          )}
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
-                <label htmlFor="mbti-select" className="block text-sm font-medium text-gray-700 mb-1" style={{ color: '#2EA9DF' }}>
+              <label htmlFor="mbti-select" className="block text-sm font-medium text-gray-700 mb-1" style={{ color: '#2EA9DF' }}>
                 MBTIタイプ
-                </label>
-                <select
+              </label>
+              <select
                 id="mbti-select"
                 value={selectedMBTI}
                 onChange={handleMBTIChange}
                 className="block w-full border py-2 px-3 shadow-sm focus:outline-none rounded-md"
                 style={{ borderColor: '#2EA9DF', backgroundColor: 'white' }}
-                >
+              >
                 <option value="">--選択してください--</option>
                 {MBTI_TYPES.map((type) => (
-                    <option key={type} value={type}>{type}</option>
+                  <option key={type} value={type}>{type}</option>
                 ))}
-                </select>
+              </select>
             </div>
             <fieldset className="mb-4">
-                <legend className="text-sm font-medium text-gray-700 mb-1" style={{ color: '#2EA9DF' }}>タイプ診断の方法</legend>
-                
-                {/* 自己診断のオプション */}
-                <label className="block mb-4"> {/* 下部の余白を追加 */}
+              <legend className="text-sm font-medium text-gray-700 mb-1" style={{ color: '#2EA9DF' }}>タイプ診断の方法</legend>
+              <label className="block mb-4">
                 <input type="radio" value="self_assessment" name="diagnosisMethod" onChange={handleDiagnosisMethodChange} className="mr-2" />
                 診断サイトでの診断を参考にしたり、書籍やWebサイトなどでMBTIに関する情報を集めて、自らの判断で決定した
-                <div className="ml-4 mt-2"> {/* インデントと上部の余白を追加 */}
-                    <a href="https://www.16personalities.com/ja/%E6%80%A7%E6%A0%BC%E8%A8%BA%E6%96%AD%E3%83%86%E3%82%B9%E3%83%88" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>16personalitiesの性格診断テスト</a>
-                    <br />
-                    <a href="https://www.idrlabs.com/jp/cognitive-function/test.php" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>心理機能テスト</a>
-                    <br />
-                    <a href="http://rinnsyou.com/archives/category/0200sinriryouhou/0203yungu" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>心理機能について</a>
-                    <br />
-                    <a href="https://www.amazon.co.jp/dp/4905050219" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>書籍</a>
+                <div className="ml-4 mt-2">
+                  <a href="https://www.16personalities.com/ja/%E6%80%A7%E6%A0%BC%E8%A8%BA%E6%96%AD%E3%83%86%E3%82%B9%E3%83%88" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>16personalitiesの性格診断テスト</a>
+                  <br />
+                  <a href="https://www.idrlabs.com/jp/cognitive-function/test.php" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>心理機能テスト</a>
+                  <br />
+                  <a href="http://rinnsyou.com/archives/category/0200sinriryouhou/0203yungu" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>心理機能について</a>
+                  <br />
+                  <a href="https://www.amazon.co.jp/dp/4905050219" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>書籍</a>
                 </div>
-                </label>
-                
-                {/* 外部評価のオプション */}
-                <label className="block">
+              </label>
+              <label className="block">
                 <input type="radio" value="official_assessment" name="diagnosisMethod" onChange={handleDiagnosisMethodChange} className="mr-2" />
                 <a href="https://www.mbti.or.jp/" target="_blank" rel="noopener noreferrer" style={{ color: '#2EA9DF' }}>公式</a>のセッションを通じて決定した
-                </label>
+              </label>
             </fieldset>
             <div className="flex justify-center gap-4">
-                <button type="submit" className="w-full inline-flex justify-center items-center px-4 py-2 font-bold rounded focus:outline-none focus:ring-opacity-50" style={{ backgroundColor: '#2EA9DF', color: 'white' }}>
+              <button type="submit" className="w-full inline-flex justify-center items-center px-4 py-2 font-bold rounded focus:outline-none focus:ring-opacity-50" style={{ backgroundColor: '#2EA9DF', color: 'white' }}>
                 更新する
-                </button>
+              </button>
             </div>
-            </form>
+          </form>
         </div>
-        </div>
+      </div>
     </>
   );
 };
