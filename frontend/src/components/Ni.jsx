7a3e1dd88ccf9f6f12674abdb5748aa8d5c74984
kentoiinuma <kento31103@icloud.com>
2024-01-20 19:46:20 +0900

    データベース

--- frontend/src/components/Ni.jsx
+++ frontend/src/components/Ni.jsx
@@ -1,4 +1,3 @@
-// frontend/src/components/Ni.jsx
 import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
