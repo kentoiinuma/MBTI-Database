e405a801a5707551c6254c4603fc8b8ba8cbe1d2
kentoiinuma <kento31103@icloud.com>
2024-06-30 06:12:44 +0900

    動的OGP

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,15 +31,17 @@ module Api
     config.autoload_paths << Rails.root.join('services')
 
     # Add middleware for rendering HTML
-    config.middleware.use ActionDispatch::Flash
-    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
+    config.middleware.delete ActionDispatch::Flash
+    config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+
+    # 必要最小限のミドルウェアのみを使用
+    config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_favorite_database_16type_session', same_site: :none, secure: Rails.env.production?
 
     # Add this line to enable HTML rendering in API mode
     config.debug_exception_response_format = :default
-
-    # Add middleware for session cookies
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_your_app_session', same_site: :none, secure: Rails.env.production?
   end
 end
