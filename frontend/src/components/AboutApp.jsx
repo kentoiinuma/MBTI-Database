fccb8767942b07d1c1326d668c504a3a35b4f23d
kentoiinuma <kento31103@icloud.com>
2024-09-08 18:18:37 +0900

    MBTIModal

--- frontend/src/components/AboutApp.jsx
+++ frontend/src/components/AboutApp.jsx
@@ -45,7 +45,6 @@ function AboutApp() {
                   href="https://mentuzzle.com/shindan/report/16type"
                   target="_blank"
                   rel="noopener noreferrer"
-                  c
                   className="text-[#2EA9DF] hover:underline"
                 >
                   おすすめの16タイプ診断サービス
